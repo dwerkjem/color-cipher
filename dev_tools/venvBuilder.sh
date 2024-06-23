d8e36db99d5abb5e2f7f32ba902e9b170e0297b7
dwerkjem <derekrneilson@gmail.com>
2024-06-23 00:29:55 -0600

    chore: Update venvBuilder.sh script to prompt user for virtual environment activation and requirement installation
    This commit modifies the venvBuilder.sh script to prompt the user whether they want to activate the virtual environment and install the requirements. It adds a while loop to continuously ask the user for input until a valid response is provided. This improvement enhances the user experience by providing more control over the virtual environment activation and requirement installation process.

--- dev_tools/venvBuilder.sh
+++ dev_tools/venvBuilder.sh
@@ -13,6 +13,14 @@ fi
 if [ -d "venv" ]
 then
     echo "venv directory already exists"
+    while true; do
+        read -p "Do you want to activate the virtual environment? (y/n): " activate_venv
+        case $activate_venv in
+            [Yy]* ) source venv/bin/activate; break;;
+            [Nn]* ) echo "Exiting without activating the virtual environment."; exit;;
+            * ) echo "Please answer yes (y) or no (n).";;
+        esac
+    done
     exit
 fi
 
@@ -22,11 +30,12 @@ python3 -m venv venv
 # Activate the virtual environment
 source venv/bin/activate
 
-# ask user if they want to install the requirements
-read -p "Do you want to install the requirements? (y/n): " install_requirements
-
-if [ "$install_requirements" == "y" ]
-then
-    # Install the requirements
-    pip install -r requirements.txt
-fi
+# Ask user if they want to install the requirements
+while true; do
+    read -p "Do you want to install the requirements? (y/n): " install_requirements
+    case $install_requirements in
+        [Yy]* ) pip install -r requirements.txt; break;;
+        [Nn]* ) echo "Requirements not installed."; break;;
+        * ) echo "Please answer yes (y) or no (n).";;
+    esac
+done
