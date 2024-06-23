f726b7a967c62c7ed4de0d42b42bf87ff999c39b
dwerkjem <derekrneilson@gmail.com>
2024-06-23 14:15:36 -0600

    chore: Add argparse for encrypt and decrypt options
    This commit adds the argparse module to the main.py file, allowing users to specify whether they want to encrypt or decrypt a message. The options include "-e" or "--encrypt" for encryption and "-d" or "--decrypt" for decryption. This enhancement provides more flexibility and control over the encryption and decryption process.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 from random import seed
+import argparse
+
 from src import tableOfValues
 
 TABLE_OF_VALUES = tableOfValues.get_table_of_values()
 
-if __name__ == '__main__':
-    for i in range(len(TABLE_OF_VALUES)):
-        print(TABLE_OF_VALUES[i])
+parser = argparse.ArgumentParser(description="Encrypt or decrypt a message with a key")
+
+# Add the arguments encrypt and decrypt and short forms of them
+parser.add_argument("-e", "--encrypt", help="Encrypt a message", action="store_true")
+parser.add_argument("-d", "--decrypt", help="Decrypt a message", action="store_true")
