112498ad17de1221ae7b1884c194a2c38668f03a
dwerkjem <derekrneilson@gmail.com>
2024-08-11 18:12:26 -0600

    Update code formatting configuration files

--- code/dev_tools/format.sh
+++ code/dev_tools/format.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Paths are relative to the root of the git repository
+
+# Run pylint on all Python files, excluding the venv directory
+find . -type f -name "*.py" ! -path "./venv/*" ! -path "./.venv/*" ! -path "./code/venv/*" -exec pylint {} +
+
+# Format Python files using black
 black .
 
+# Get list of files that have been added, copied, modified, or renamed
 FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
 [ -z "$FILES" ] && exit 0
 
