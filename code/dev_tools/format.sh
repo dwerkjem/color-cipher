9c6398aee5b289118b19c81fbdaf26d96070e852
dwerkjem <derekrneilson@gmail.com>
2024-08-11 18:25:51 -0600

    now enforcing at least 9/10 pytlint score

--- code/dev_tools/format.sh
+++ code/dev_tools/format.sh
@@ -3,7 +3,18 @@
 # Paths are relative to the root of the git repository
 
 # Run pylint on all Python files, excluding the venv directory and __init__.py files
-find . -type f -name "*.py" ! -name "__init__.py" ! -path "./venv/*" ! -path "./.venv/*" ! -path "./code/venv/*" -exec pylint {} +
+pylint_output=$(find . -type f -name "*.py" ! -name "__init__.py" ! -path "./venv/*" ! -path "./.venv/*" ! -path "./code/venv/*" -exec pylint {} +)
+pylint_score=$(echo "$pylint_output" | grep "Your code has been rated at" | awk '{print $7}' | cut -d'/' -f1)
+
+# Check pylint score
+if (( $(echo "$pylint_score < 9.0" | bc -l) )); then
+  echo "Pylint score is below 9.0: $pylint_score"
+  echo "$pylint_output"
+  exit 1
+else
+  echo "Pylint score is 9.0 or above: $pylint_score"
+  echo "$pylint_output"
+fi
 
 # Format Python files using black
 black .
